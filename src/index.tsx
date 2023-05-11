36317ef612aa9de6ebabb29f41d2c10c57be22a5
jurassicplayer <jurassicplayer75@gmail.com>
2023-05-11 11:57:54 -0700

    Try url query params

--- src/index.tsx
+++ src/index.tsx
@@ -10,39 +10,19 @@ import {
   ServerAPI,
   showContextMenu,
   staticClasses,
-} from "decky-frontend-lib";
-import { VFC } from "react";
-import { FaShip } from "react-icons/fa";
+} from "decky-frontend-lib"
+import { ReactElement, VFC } from "react"
+import { FaShip } from "react-icons/fa"
 
-import logo from "../assets/logo.png";
-
-// interface AddMethodArgs {
-//   left: number;
-//   right: number;
-// }
+import logo from "../assets/logo.png"
 
 const Content: VFC<{ serverAPI: ServerAPI }> = ({}) => {
-  // const [result, setResult] = useState<number | undefined>();
-
-  // const onClick = async () => {
-  //   const result = await serverAPI.callPluginMethod<AddMethodArgs, number>(
-  //     "add",
-  //     {
-  //       left: 2,
-  //       right: 2,
-  //     }
-  //   );
-  //   if (result.success) {
-  //     setResult(result.result);
-  //   }
-  // };
-
   return (
     <PanelSection title="Panel Section">
       <PanelSectionRow>
         <ButtonItem
           layout="below"
-          onClick={(e) =>
+          onClick={(e: Event) =>
             showContextMenu(
               <Menu label="Menu" cancelText="CAAAANCEL" onCancel={() => {}}>
                 <MenuItem onSelected={() => {}}>Item #1</MenuItem>
@@ -67,8 +47,8 @@ const Content: VFC<{ serverAPI: ServerAPI }> = ({}) => {
         <ButtonItem
           layout="below"
           onClick={() => {
-            Router.CloseSideMenus();
-            Router.Navigate("/decky-plugin-test");
+            Router.CloseSideMenus()
+            Router.Navigate("/decky-plugin/view?name=DeckyPluginRouterTest&arg02=othertext")
           }}
         >
           Router
@@ -89,17 +69,26 @@ const DeckyPluginRouterTest: VFC = () => {
   );
 };
 
+const QueryParamsTest: VFC = () => {
+  const searchParams = new URLSearchParams(document.location.search)
+  let name = searchParams.get("name")
+  let node: ReactElement|null = <div>Missing view name</div>
+  if (name = 'DeckyPluginRouterTest') {
+    node = DeckyPluginRouterTest({})
+  }
+  return node
+}
+
+
 export default definePlugin((serverApi: ServerAPI) => {
-  serverApi.routerHook.addRoute("/decky-plugin-test", DeckyPluginRouterTest, {
-    exact: true,
-  });
+  serverApi.routerHook.addRoute("/decky-plugin/view", QueryParamsTest);
 
   return {
     title: <div className={staticClasses.Title}>Example Plugin</div>,
     content: <Content serverAPI={serverApi} />,
     icon: <FaShip />,
     onDismount() {
-      serverApi.routerHook.removeRoute("/decky-plugin-test");
+      serverApi.routerHook.removeRoute("/decky-plugin/view")
     },
   };
 });
